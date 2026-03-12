60f758af357f4c293cfd13a0c6835bab7c1dfa65
Neikkie <neikkie21@outlook.com>
2026-03-12 04:28:18 -0400

    Initial Commit

---
+++ Travel Buddy/Travel_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_BuddyApp.swift
+//  Travel Buddy
+//
+//  Created by Shanique Beckford on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Travel_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
